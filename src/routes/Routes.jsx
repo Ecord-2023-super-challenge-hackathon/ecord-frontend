4a36106b5c8dde6fbcbdb3a675cd84037ffa138b
pakxe <pigkill40@naver.com>
2023-01-29 01:50:19 +0900

    feat(Routes): Login 페이지 라우터 연결

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main/Main';
+import Login from '../pages/Login/Login';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Main />} />
+      <Route exact path='/login' element={<Login />} />
     </Routes>
   </BrowserRouter>
 );
