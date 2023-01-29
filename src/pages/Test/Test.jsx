29cc44a38f2c12a9901a605590aab87d41e760cc
pakxe <pigkill40@naver.com>
2023-01-30 02:20:59 +0900

    feat(Test): 스티커로 꾸민 영수증을 저장하는 기능 추가

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Layout from '../../component/Layout/Layout';
 import axios from 'axios';
 import Stickers from '../../component/Stickers/Stickers';
 import styled from 'styled-components';
 import { motion, useDragControls } from 'framer-motion';
+import domToImage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 const Sticker = styled(motion.img)`
   cursor: pointer;
@@ -19,6 +21,7 @@ const Test = () => {
   const [stickerOnList, setStickerOnList] = useState([]);
   const [onSticker, setOnSticker] = useState(false);
   const controls = useDragControls();
+  const cardRef = useRef();
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API}/sticker/load`).then((r) => {
@@ -28,38 +31,55 @@ const Test = () => {
     });
   }, []);
 
+  const onDownloadBtn = () => {
+    const card = cardRef.current;
+    const filter = (card) => {
+      return card.tagName !== 'BUTTON';
+    };
+    domToImage.toBlob(card, { filter: filter }).then((blob) => {
+      saveAs(blob, 'card.png');
+    });
+  };
+
   return (
     <>
       <Layout>
-        <Stickers
-          onSticker={onSticker}
-          setOnSticker={setOnSticker}
-          stickerList={stickerList}
-          setStickerOnList={setStickerOnList}
-          stickerOnList={stickerOnList}
-        />
-        <button type='button' onClick={() => setOnSticker(!onSticker)}>
-          스티커 키자
-        </button>
-        {stickerOnList.map(
-          (a, i) =>
-            a && (
-              <Sticker
-                dragMomentum={false}
-                dragElastic={0.1}
-                whileDrag={{ scale: 1.2 }}
-                drag
-                onDrag={(event, info) => console.log(info.offset.x, info.offset.y)}
-                dragControls={controls}
-                whileTap={{ cursor: 'grabbing' }}
-                key={i}
-                src={`${process.env.REACT_APP_API}${stickerList[i].sticker_url}`}
-              />
-            ),
-        )}
+        <div className='card' ref={cardRef} style={{ position: 'relative' }}>
+          <Stickers
+            onSticker={onSticker}
+            setOnSticker={setOnSticker}
+            stickerList={stickerList}
+            setStickerOnList={setStickerOnList}
+            stickerOnList={stickerOnList}
+          />
+          <button type='button' onClick={() => setOnSticker(!onSticker)}>
+            스티커 키자
+          </button>
+          {stickerOnList.map(
+            (a, i) =>
+              a && (
+                <Sticker
+                  dragMomentum={false}
+                  dragElastic={0.1}
+                  whileDrag={{ scale: 1.2 }}
+                  drag
+                  // onDrag={(event, info) => console.log(info.offset.x, info.offset.y)}
+                  dragControls={controls}
+                  whileTap={{ cursor: 'grabbing' }}
+                  key={i}
+                  src={`${process.env.REACT_APP_API}${stickerList[i].sticker_url}`}
+                />
+              ),
+          )}
+          <button className='downBtn' onClick={onDownloadBtn}>
+            이미지 다운로드
+          </button>
+        </div>
       </Layout>
     </>
   );
 };
 
 export default Test;
+
+// dom to image에서 스티커 캡쳐를 하려면 자식(스티커)는 absolute니까 부모에게 relative를 줘야한다.
