029eacf1ad0664ff00a60e5014ce8f3544febdc9
pakxe <pigkill40@naver.com>
2023-01-29 18:03:02 +0900

    feat(Menus): 영수증에 들어가는 Menus 컴포넌트 완성

---
+++ src/component/Recetpt/Menus.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import Typography from '../Typography/Typography';
+
+const MenuWrapper = styled.div`
+  display: flex;
+  height: 18px;
+  justify-content: space-between;
+`;
+
+const LeftMenu = styled.div`
+  width: 67px;
+`;
+
+const RightMenu = styled.div`
+  width: 49px;
+  display: flex;
+  justify-content: end;
+`;
+
+const OtherMenus = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+`;
+
+const Menus = ({ menus, ea }) => {
+  return (
+    <>
+      {menus.slice(0, 3).map((menuName, i) => (
+        <MenuWrapper>
+          <LeftMenu>
+            <Typography SmallText>{menuName}</Typography>
+          </LeftMenu>
+          <RightMenu>
+            <Typography SmallText>…… X{ea[i]}</Typography>
+          </RightMenu>
+        </MenuWrapper>
+      ))}
+      {menus.length > 3 && (
+        <OtherMenus>
+          <Typography receiptSmall>외 {menus.length - 3}건</Typography>
+        </OtherMenus>
+      )}
+    </>
+  );
+};
+
+export default Menus;
