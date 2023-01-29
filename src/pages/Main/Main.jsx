a384b98aa5cae7b0c4c7c9463619b588d71a2624
pakxe <pigkill40@naver.com>
2023-01-29 18:03:45 +0900

    feat(Main): 영수증 불러오는 aioxs 연결

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../component/Layout/Layout';
 import Button from '../../component/Button/Button';
 import FooterNavigate from '../../component/FooterNavigate';
 import { ReactComponent as Ecord_Title } from '../../assets/svg/Ecord_Title.svg';
 import { ReactComponent as Ecord } from '../../assets/svg/Ecord.svg';
 import { ReactComponent as Intro_Image } from '../../assets/svg/Intro_Image.svg';
+import Receipt from '../../component/Recetpt/Receipt';
+import axios from 'axios';
 
 const Main = () => {
+  const [receipts, setReceipts] = useState([]);
+
+  useEffect(() => {
+    const user_index = localStorage.getItem('user_index');
+    axios
+      .get(`${process.env.REACT_APP_API}/users/${user_index}/receipts`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then((r) => {
+        console.log(r.data);
+        setReceipts(r.data);
+      });
+  }, []);
+
   return (
     <Layout>
-      <Ecord_Title />
-      <Ecord />
-      <Intro_Image />
-      <Button hover>시작하기</Button>
-      <div>Main dd</div>
       <FooterNavigate />
+      {receipts.map((data) => {
+        console.log(data);
+        return <Receipt data={data} key={data.receipt_index} />;
+      })}
     </Layout>
   );
 };
