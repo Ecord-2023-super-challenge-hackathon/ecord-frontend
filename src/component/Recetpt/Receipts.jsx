d1fcd6670e4f2fc37ac0ecc7c0ca532eb2fc58e1
pakxe <pigkill40@naver.com>
2023-01-29 18:24:48 +0900

    feat(Receipts): 영수증을 담아서 가로로 뿌리는 Receipts 컴포넌트 완성

---
+++ src/component/Recetpt/Receipts.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import Receipt from './Receipt';
+
+const ReceiptContainer = styled.div`
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  gap: 15px;
+
+  padding: 20px;
+`;
+
+const Receipts = ({ receipts }) => (
+  <ReceiptContainer>
+    {receipts.map((data) => {
+      console.log(data);
+      return <Receipt data={data} key={data.receipt_index} />;
+    })}
+  </ReceiptContainer>
+);
+
+export default Receipts;
