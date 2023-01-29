8292388750a7f11d3c087a9b358e26d119c16cab
sean2337 <sean2337@naver.com>
2023-01-29 17:32:17 +0900

    feat:SummaryInfo영역 생성

--- src/pages/Main/SummaryInfo.jsx
+++ src/pages/Main/SummaryInfo.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Typography from '../../component/Typography/Typography';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 //요약 정보를 담는 전체적인 Wrapper
 const Wrapper = styled.div`
@@ -8,6 +10,8 @@ const Wrapper = styled.div`
   height: 260px;
   border-bottom: 6px solid #eff4f5;
 `;
+
+//인사말과 관련한 컴포넌트 위치 조정해주는 Wrapper
 const TypoWrapper = styled.div`
   width: 100%;
   height: auto;
@@ -15,9 +19,53 @@ const TypoWrapper = styled.div`
   padding-top: 50px;
 `;
 
+// 인사말 중에 두번째 줄에 글자 줄 맞추기 위한 Wrapper
+const CarbonTypoWrapper = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+`;
+
+// 환급금은 관련된 정보를 보여줄 Box
+const InfoBox = styled.div`
+  width: 316px;
+  height: 91px;
+  margin-top: 25px;
+  background-color: #05be70;
+  border-radius: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+const InfoBoxTypoWrapper = styled.div`
+  width: 220px;
+  height: auto;
+  padding-left: 20px;
+
+  display: flex;
+  flex-wrap: wrap;
+  line-height: 23px;
+`;
+
+const CircleProgressBarWrapper = styled.div`
+  width: 47px;
+  height: 47px;
+  margin-right: 26px;
+  font-weight: 700;
+  font-size: 10px;
+  text-align: center;
+`;
+
 const SummaryInfo = () => {
-  var userName = '지애';
-  var carbonEmissions = 45;
+  //-> 이부분만 axoios로 값들 가져와서 바꿔주면 될듯
+  // 이용자 이름
+  const [userName, setUserName] = useState('지애');
+  // 탄소 배출 막은 무게
+  const [carbonEmissions, setCarbonEmissions] = useState(45);
+  // 환급까지 남은 횟수
+  const [issuances, setIssuances] = useState(4);
+  // progressBar 퍼센트
+  const [percentage, setPercentage] = useState(70);
 
   return (
     <>
@@ -26,14 +74,45 @@ const SummaryInfo = () => {
           <Typography MainTitle fontSize='19px'>
             {userName}님, 환영해요!
           </Typography>
-
-          <Typography MainTitle fontSize='19px'>
-            이번 달{' '}
+          <CarbonTypoWrapper>
+            <Typography MainTitle fontSize='19px'>
+              이번 달
+            </Typography>
             <Typography MainTitle fontSize='19px' color='mainGreen'>
+              <span style={{ paddingLeft: '4px' }} />
               {carbonEmissions}g의 탄소 배출
             </Typography>
-            을 막았어요
-          </Typography>
+            <Typography MainTitle fontSize='19px'>
+              을 막았어요
+            </Typography>
+          </CarbonTypoWrapper>
+          <InfoBox>
+            <InfoBoxTypoWrapper>
+              <Typography ButtonText color='white'>
+                1000원 환급<span style={{ opacity: '0.7' }}>까지</span>
+              </Typography>
+              <Typography ButtonText color='white'>
+                <span style={{ opacity: '0.7' }}>앞으로</span> {issuances}회 발급{' '}
+                <span style={{ opacity: '0.7' }}>남았어요!</span>
+              </Typography>
+            </InfoBoxTypoWrapper>
+            <CircleProgressBarWrapper>
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                strokeWidth={9}
+                styles={buildStyles({
+                  strokeLinecap: 'butt',
+                  pathTransition: 'none',
+
+                  pathColor: `rgb(255,255,255,${percentage / 100})`,
+                  textColor: '#FFFFFF',
+                  trailColor: `rgb(255,255,255,${percentage / 100})`,
+                  backgroundColor: '#FFFFFF',
+                })}
+              />
+            </CircleProgressBarWrapper>
+          </InfoBox>
         </TypoWrapper>
       </Wrapper>
     </>
