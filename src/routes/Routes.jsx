ffe9f495a67c6789502d3578e7179bb9669f5859
sean2337 <sean2337@naver.com>
2023-01-29 13:38:35 +0900

    chore:main 충돌 수정

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main/Main';
-import Footer_Navigate from '../assets/theme/Footer_Navigate';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Main />} />
-      <Route exact path='/FooterNavigate' element={<Main />} />
     </Routes>
   </BrowserRouter>
 );
