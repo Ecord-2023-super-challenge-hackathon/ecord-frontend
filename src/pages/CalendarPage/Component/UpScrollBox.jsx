1d852af469a3bec236d101402d37c1607ce7f87d
sean2337 <sean2337@naver.com>
2023-01-30 14:15:13 +0900

    feat:UpscrollBox 바 생성

---
+++ src/pages/CalendarPage/Component/UpScrollBox.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Typography from '../../../component/Typography/Typography';
+import styled, { keyframes } from 'styled-components';
+import { IoIosArrowUp } from 'react-icons/io';
+const Wrapper = styled.div`
+  width: 100%;
+  height: 45px;
+  position: fixed;
+  bottom: 78px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  width: 360px;
+  height: 45px;
+  background-color: #05be70;
+  border-radius: 16px 16px 0px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UpScrollBox = () => {
+  return (
+    <>
+      <Wrapper>
+        <Box>
+          <Typography contentText color='backgroundWhite'>
+            2022.01.12(화) 확인하기
+          </Typography>
+          <IoIosArrowUp size={25} style={{ color: 'white' }} />
+        </Box>
+      </Wrapper>
+    </>
+  );
+};
+
+export default UpScrollBox;
